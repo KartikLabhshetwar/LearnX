a577d8fa2f12acb20f843b434aeca4ba2bb2f332
Kartik Labhshetwar <kartik.labhshetwar@gmail.com>
2024-09-05 02:32:37 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,11 @@ const cors = require("cors")
 const mainRoute = require("./routes/index")
 const dotenv = require("dotenv")
 
-
+const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-const app = express();
+
 dotenv.config();
 
 app.use("/api/v1", mainRoute)
